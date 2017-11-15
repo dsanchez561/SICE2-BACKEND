8ec9c58223b827356ab144e42a4835665b05c62b
dsanchez561 <33670777+dsanchez561@users.noreply.github.com>
2017-11-15 09:45:37 -0500

    COMMIT: Primer commit

---
+++ src/main/java/co/com/javeriana/SIEEJ/SieejApplication.java
@@ -0,0 +1,12 @@
+package co.com.javeriana.SIEEJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SieejApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SieejApplication.class, args);
+	}
+}
---
+++ src/test/java/co/com/javeriana/SIEEJ/SieejApplicationTests.java
@@ -0,0 +1,16 @@
+package co.com.javeriana.SIEEJ;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SieejApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
