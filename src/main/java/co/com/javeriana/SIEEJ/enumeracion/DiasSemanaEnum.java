b268dfa50013581098a99fbb96c7b92d5b655beb
WO\DANIELSANCHEZ <dsanchez561@hotmail.com>
2018-01-27 08:38:31 -0500

    COMMIT: Se agregan nuevas entidades al sistema

---
+++ src/main/java/co/com/javeriana/SIEEJ/enumeracion/DiasSemanaEnum.java
@@ -0,0 +1,17 @@
+package co.com.javeriana.SIEEJ.enumeracion;
+
+/**
+ * Enumera los niveles (estados) de los usuarios del sistema
+ * @author Javeriana
+ */
+public enum DiasSemanaEnum {
+	LUNES,
+	MARTES,
+	MIERCOLES,
+	JUEVES,
+	VIERNES,
+	SABADO,
+	DOMINGO;
+}
+
+
