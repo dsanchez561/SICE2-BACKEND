5420989a4f0f9b1c979338e533dfbfb1d1175967
WO\brayanguerrero <brayanguerrero@worldofficeonline.com>
2018-01-27 09:28:57 -0500

    COMMIT cambios relativos al asociar etiquetas

--- src/main/java/co/com/javeriana/SIEEJ/rest/RestUsuario.java
+++ src/main/java/co/com/javeriana/SIEEJ/rest/RestUsuario.java
@@ -1,6 +1,7 @@
 package co.com.javeriana.SIEEJ.rest;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,12 +9,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.com.javeriana.SIEEJ.entidades.Etiqueta;
 import co.com.javeriana.SIEEJ.entidades.Usuario;
+import co.com.javeriana.SIEEJ.implementacion.UsuarioImpl;
 import co.com.javeriana.SIEEJ.repositories.UsuarioRepository;
 import co.com.javeriana.SIEEJ.seguridad.ConfiguracionSeguridad;
 
@@ -28,6 +32,9 @@ public class RestUsuario {
 	
 	@Autowired
 	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
+	private UsuarioImpl usuarioImpl;
 
 	/**
 	 * Metodo que permite listar los usuarios
@@ -45,6 +52,15 @@ public class RestUsuario {
 		}
 	}
 	
+	
+	@RequestMapping(value="/asociarEtiquetas/{id}",method=RequestMethod.POST)
+	public ResponseEntity<Usuario> asociarEtiquetas(@PathVariable("id") String id, @RequestBody List<Etiqueta> etiquetas){
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(usuarioImpl.asociarEtiquetas(id, etiquetas));
+		}catch(Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+	}
 	/**
 	 * Metodo que permite asociar tipos de proyectos que le interesan al usuario
 	 * 
