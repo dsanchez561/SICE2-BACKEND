0554f8260e8eadf4b8e081cf909e4852943419dd
Daniel Sanc <Daniel Sanc@MSI-DANIEL>
2018-05-22 21:18:08 -0500

    COMMIT: Se corrige eliminar servicio

--- sice-repositorios/src/main/java/co/com/javeriana/SICE2/repositories/ServicioRepository.java
+++ sice-repositorios/src/main/java/co/com/javeriana/SICE2/repositories/ServicioRepository.java
@@ -4,6 +4,7 @@
 package co.com.javeriana.SICE2.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import co.com.javeriana.SICE2.model.general.Dominio;
 import co.com.javeriana.SICE2.model.general.Servicio;
@@ -12,6 +13,7 @@ import co.com.javeriana.SICE2.model.general.Servicio;
  * @author Javeriana
  *
  */
+@Repository(value="Servicio")
 public interface ServicioRepository extends JpaRepository<Servicio, Long> {
 
 	Servicio findByDominioAndNombre(Dominio dominio, String pagina);
